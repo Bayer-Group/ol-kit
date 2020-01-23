8dbbaa24147ea6881969b50e4b4a4c32c5683f5f
Jake <jwebb@1904labs.com>
2020-01-23 17:20:47 -0600

    tests running & passing

--- src/Controls/Controls.test.js
+++ src/Controls/Controls.test.js
@@ -1,10 +1,12 @@
+import React from 'react'
 import { mount } from 'enzyme'
 import olMap from 'ol/map'
 import Controls from './Controls'
 
 describe('<Controls />', () => {
   it('should mount', () => {
-    const mockMap = new olMap()
+    document.body.innerHTML = `<div id='map'></div>`
+    const mockMap = new olMap({ target: 'map' })
     const wrapper = mount(<Controls map={mockMap} />)
 
     console.log(wrapper)
--- src/Map/utils.test.js
+++ src/Map/utils.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { mount } from 'enzyme'
 import olMap from 'ol/map'
+import Map from './Map'
 import { connectToMap, createMap } from './utils'
 
 describe('Map utils', () => {
