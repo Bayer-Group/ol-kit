a00b5b7317b1c82ffc25cb31d51f5e5c591d7676
Jake Stäzrad <jakewebbsite@gmail.com>
2020-02-03 14:46:47 -0600

    update lint config

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ import * as ol_kit from './index' // eslint-disable-line camelcase
 describe('ol-kit package', () => {
   it('should not export undefined', () => {
     Object.keys(ol_kit).forEach(exportKey => {
-      expect(ol_kit[exportKey]).not.toBeUndefined()
+      expect(Boolean(ol_kit[exportKey])).not.toBeUndefined()
     })
 
     expect(ol_kit).toMatchSnapshot()
