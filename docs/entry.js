419a2521bc37618d0736b6f6d0a12a1dc0d934bb
Glen Selle <glen.selle.ext@bayer.com>
2020-03-17 10:40:14 -0500

    Added a new i18n tutorial to explain how translations work

--- docs/entry.js
+++ docs/entry.js
@@ -22,5 +22,23 @@
 
     import './styles/iframe.css';
 
-  import Component0 from '../src/Map/Map.js';
-reactComponents['Map'] = Component0;
+  import Component0 from '../src/Controls/styled.js';
+reactComponents['CompassContainer'] = Component0;
+
+import Component1 from '../src/Basemaps/styled.js';
+reactComponents['Container'] = Component1;
+
+import Component2 from '../src/Controls/styled.js';
+reactComponents['ControlsContainer'] = Component2;
+
+import Component3 from '../src/Controls/styled.js';
+reactComponents['Icon'] = Component3;
+
+import Component4 from '../src/Controls/styled.js';
+reactComponents['IconsContainer'] = Component4;
+
+import Component5 from '../src/Controls/styled.js';
+reactComponents['IconSeparator'] = Component5;
+
+import Component6 from '../src/Map/Map.js';
+reactComponents['Map'] = Component6;
