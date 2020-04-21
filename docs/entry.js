3d2e382f36fdcdf7d78775fb913dfda8d22c6c2d
Jake Stäzrad <jakewebbsite@gmail.com>
2020-04-21 11:46:26 -0500

    update docs

--- docs/entry.js
+++ docs/entry.js
@@ -22,65 +22,65 @@
 
     import './styles/iframe.css';
 
-  import Component0 from '../src/Basemaps/BasemapManager.js';
-reactComponents['BasemapManager'] = Component0;
+  import Component0 from '../src/Basemaps/BingMaps.js';
+reactComponents['BasemapBingMaps'] = Component0;
 
-import Component1 from '../src/Basemaps/BingMaps.js';
-reactComponents['BingMaps'] = Component1;
+import Component1 from '../src/Basemaps/BlankWhite.js';
+reactComponents['BasemapBlankWhite'] = Component1;
 
-import Component2 from '../src/Basemaps/BlankWhite.js';
-reactComponents['BlankWhite'] = Component2;
+import Component2 from '../src/Basemaps/BasemapManager.js';
+reactComponents['BasemapManager'] = Component2;
 
-import Component3 from '../src/Controls/Compass.js';
-reactComponents['Compass'] = Component3;
+import Component3 from '../src/Basemaps/OpenStreetMap.js';
+reactComponents['BasemapOpenStreetMap'] = Component3;
 
-import Component4 from '../src/Controls/Controls.js';
-reactComponents['Controls'] = Component4;
+import Component4 from '../src/Basemaps/StamenTerrain.js';
+reactComponents['BasemapStamenTerrain'] = Component4;
 
-import Component5 from '../src/Map/Map.js';
-reactComponents['Map'] = Component5;
+import Component5 from '../src/Basemaps/StamenTonerDark.js';
+reactComponents['BasemapStamenTonerDark'] = Component5;
 
-import Component6 from '../src/Basemaps/OpenStreetMap.js';
-reactComponents['OpenStreetMap'] = Component6;
+import Component6 from '../src/Basemaps/StamenTonerLite.js';
+reactComponents['BasemapStamenTonerLite'] = Component6;
 
-import Component7 from '../src/Popup/Popup.js';
-reactComponents['Popup'] = Component7;
+import Component7 from '../src/Controls/Compass.js';
+reactComponents['Compass'] = Component7;
 
-import Component8 from '../src/Popup/PopupInsert/PopupActionGroup/index.js';
-reactComponents['PopupActionGroup'] = Component8;
+import Component8 from '../src/Controls/Controls.js';
+reactComponents['Controls'] = Component8;
 
-import Component9 from '../src/Popup/PopupInsert/PopupActionItem/index.js';
-reactComponents['PopupActionItem'] = Component9;
+import Component9 from '../src/Map/Map.js';
+reactComponents['Map'] = Component9;
 
-import Component10 from '../src/Popup/PopupBase.js';
-reactComponents['PopupBase'] = Component10;
+import Component10 from '../src/Popup/Popup.js';
+reactComponents['Popup'] = Component10;
 
-import Component11 from '../src/Popup/PopupInsert/PopupDataList/index.js';
-reactComponents['PopupDataList'] = Component11;
+import Component11 from '../src/Popup/PopupInsert/PopupActionGroup/index.js';
+reactComponents['PopupActionGroup'] = Component11;
 
-import Component12 from '../src/Popup/PopupInsert/PopupDefaultInsert.js';
-reactComponents['PopupDefaultInsert'] = Component12;
+import Component12 from '../src/Popup/PopupInsert/PopupActionItem/index.js';
+reactComponents['PopupActionItem'] = Component12;
 
-import Component13 from '../src/Popup/PopupInsert/PopupDefaultPage/index.js';
-reactComponents['PopupDefaultPage'] = Component13;
+import Component13 from '../src/Popup/PopupBase.js';
+reactComponents['PopupBase'] = Component13;
 
-import Component14 from '../src/Popup/PopupInsert/PopupPageLayout/index.js';
-reactComponents['PopupPageLayout'] = Component14;
+import Component14 from '../src/Popup/PopupInsert/PopupDataList/index.js';
+reactComponents['PopupDataList'] = Component14;
 
-import Component15 from '../src/Popup/PopupInsert/PopupPageLayoutChild/index.js';
-reactComponents['PopupPageLayoutChild'] = Component15;
+import Component15 from '../src/Popup/PopupInsert/PopupDefaultInsert.js';
+reactComponents['PopupDefaultInsert'] = Component15;
 
-import Component16 from '../src/Popup/PopupInsert/PopupTabs/index.js';
-reactComponents['PopupTabs'] = Component16;
+import Component16 from '../src/Popup/PopupInsert/PopupDefaultPage/index.js';
+reactComponents['PopupDefaultPage'] = Component16;
 
-import Component17 from '../src/Basemaps/StamenTerrain.js';
-reactComponents['StamenTerrain'] = Component17;
+import Component17 from '../src/Popup/PopupInsert/PopupPageLayout/index.js';
+reactComponents['PopupPageLayout'] = Component17;
 
-import Component18 from '../src/Basemaps/StamenTonerDark.js';
-reactComponents['StamenTonerDark'] = Component18;
+import Component18 from '../src/Popup/PopupInsert/PopupPageLayoutChild/index.js';
+reactComponents['PopupPageLayoutChild'] = Component18;
 
-import Component19 from '../src/Basemaps/StamenTonerLite.js';
-reactComponents['StamenTonerLite'] = Component19;
+import Component19 from '../src/Popup/PopupInsert/PopupTabs/index.js';
+reactComponents['PopupTabs'] = Component19;
 
 import Component20 from '../src/Controls/ZoomControls.js';
 reactComponents['ZoomControls'] = Component20;
