a4993ef7c97b62472b50467e5b27edadebc74a58
Noah Tatko <tatkon@rbtrainers.com>
2020-07-02 09:19:31 -0500

    only show scrollbars in layer panel when needed

--- src/LayerPanel/LayerPanelContent/styled.js
+++ src/LayerPanel/LayerPanelContent/styled.js
@@ -5,7 +5,7 @@ import { connectToMap } from 'Map'
 
 export const CardContent = connectToMap(styled(({ ...props }) => <MaterialCardContent {...props} />)({
   maxHeight: '565px',
-  overflow: 'hidden',
+  overflow: 'auto',
   padding: props => props.padding ? props.padding : '10px 15px',
   marginBottom: '5px'
 }))
