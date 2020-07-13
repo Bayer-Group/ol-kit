910683f1b2ad59f8438cd8904a8cd020d1658a36
MOULDEN, PATRICK [AG-Contractor/1000] <patrick.moulden.ext@bayer.com>
2020-07-13 14:32:35 -0400

    initial dnd replacement

--- src/LayerPanel/LayerPanelList/index.js
+++ src/LayerPanel/LayerPanelList/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import List from '@material-ui/core/List'
 import LayerPanelListItem from 'LayerPanel/LayerPanelListItem'
-import { Container, Draggable } from 'react-smooth-dnd'
+import nanoid from 'nanoid'
 
 import PropTypes from 'prop-types'
 
@@ -32,7 +32,8 @@ const applyDrag = (arr, dragResult) => {
  * @since 0.5.0
  */
 class LayerPanelList extends Component {
-  onDrop = e => {
+  handleDrop = e => {
+    console.log('drop', e)
     const { onSort, onReorderedItems, items } = this.props
     const reorderedItems = applyDrag(items.sort(onSort), e)
 
@@ -41,29 +42,94 @@ class LayerPanelList extends Component {
     }
   }
 
+  onDrop = e => {
+    e.preventDefault()
+    e.dataTransfer.getData('application/ol-kit')
+
+    // console.log(e.target) // eslint-disable-line no-console
+    // console.log(e.dataTransfer.getData('application/ol-kit')) // eslint-disable-line no-console
+    // console.log(document.getElementById(e.dataTransfer.getData('application/ol-kit'))) // eslint-disable-line no-console
+
+    let dropNode = e.target
+
+    do {
+      if (dropNode.className === 'draggable') {
+        console.log(dropNode) // eslint-disable-line no-console
+
+        const removedIndex = dropNode.id[0]
+        const payload = document.getElementById(e.dataTransfer.getData('application/ol-kit'))
+        const addedIndex = payload.id[0]
+
+        this.handleDrop({ ...e, removedIndex, addedIndex, payload })
+
+        break
+      }
+    }
+    while (dropNode = dropNode.parentNode, dropNode.id !== 'dragContainer')
+
+  }
+
+  onDragOver = e => {
+    e.preventDefault()
+    e.dataTransfer.dropEffect = 'move'
+    let dropNode = e.target
+
+    do {
+      if (dropNode.className === 'dropzone') {
+
+        this.dragTarget.parentNode.replaceChild(dropNode.firstChild, this.dragTarget)
+        dropNode.appendChild(this.dragTarget)
+
+        break
+      }
+    }
+    while (dropNode = dropNode.parentNode, dropNode.id !== 'dragContainer')
+  }
+
+  onDragStart = e => {
+    e.dataTransfer.dropEffect = 'move'
+    console.log(e.target) // eslint-disable-line no-console
+    e.dataTransfer.setData('application/ol-kit', e.target.id)
+    this.dragTarget = e.target
+  }
+
   render () {
     const { children, disableDrag, items } = this.props
-    const ItemContainer = disableDrag ? 'div' : Draggable
+    // const ItemContainer = disableDrag ? 'div' : Draggable
 
     if (children) {
       return (
         <List>
-          <Container onDrop={this.onDrop}>
-            {React.Children.map(this.props.children, child => <ItemContainer key={child.id}>{child}</ItemContainer>)}
-          </Container>
+          <div id='dragContainer'>
+            {React.Children.map(this.props.children, (child, i) => {
+              const id = `${i}${nanoid(6)}`
+
+              return (
+                <div className={`dropzone`} onDrop={this.onDrop} onDragOver={this.onDragOver} key={id || child.id}>
+                  <div id={id} className={'draggable'} draggable={disableDrag ? 'false' : 'true'} onDragStart={this.onDragStart}>{child}</div>
+                </div>
+              )
+            })}
+          </div>
         </List>
       )
     } else if (items) {
       return (
         <List>
-          <Container onDrop={this.onDrop} >
-            {items.map(item => (
-              <ItemContainer key={item}>
-                <LayerPanelListItem>{item}</LayerPanelListItem>
-              </ItemContainer>
-            )
+          <div id='dragContainer'>
+            {items.map((item, i) => {
+              const id = `${i}${nanoid(6)}`
+
+              return (
+                <div className={`dropzone`} onDrop={this.onDrop} onDragOver={this.onDragOver} key={item}>
+                  <div id={id} className={'draggable'} draggable={disableDrag ? 'false' : 'true'} onDragStart={this.onDragStart}>
+                    <LayerPanelListItem>{item}</LayerPanelListItem>
+                  </div>
+                </div>
+              )
+            }
             )}
-          </Container>
+          </div>
         </List>
       )
     } else {
