c1a137786e8fb5a30c77122a5d9a9d92aec5276b
MOULDEN, PATRICK [AG-Contractor/1000] <patrick.moulden.ext@bayer.com>
2020-09-04 12:32:32 -0400

    integration test runner improvements

--- app/integration.sh
+++ app/integration.sh
@@ -8,8 +8,8 @@ cleanup() {
 	for pid in "${pids[@]}"; do
 		kill -0 "$pid" && kill "$pid" # kill process only if it's still running
 	done
-
 	exit 0
+	printf "/nAll Done!/n"
 }
 # and set that function to run before we exit, or specifically when we get a SIGTERM
 trap cleanup EXIT TERM
