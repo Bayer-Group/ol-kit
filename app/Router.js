a1a2a66ee839e83a911e7ced84776011ed4a5f54
Jake Stäzrad <jakewebbsite@gmail.com>
2020-09-16 16:26:06 -0500

    add a snackbar

--- app/Router.js
+++ app/Router.js
@@ -9,6 +9,9 @@ import {
   useParams
 } from 'react-router-dom'
 
+import Snackbar from './Snackbar'
+
+// routes
 import Demo from './routes/demo/App'
 import Space from './routes/space/App'
 
@@ -20,7 +23,7 @@ const Nav = styled.div`
   font-size: 16px;
   opacity: .91;
   background: white;
-  width: 400px;
+  width: 250px;
   margin: 0;
   top: 0;
   right: 0;
@@ -31,25 +34,30 @@ const Nav = styled.div`
   height: 55px;
   align-items: center;
   font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-  border-radius: 5px;
+  border-radius: 2px;
 `
 const NavLink = styled.div`
   margin: 10px;
+`
+const NavImg = styled.img`
+  height: 50px;
+  width: 50px;
 `
 
 export default function App() {
   return (
     <Router>
+      <Snackbar />
       <Nav>
-        Check out these ol-kit demos:
+        ol-kit demos:
         <Link to='/'>
           <NavLink>
-            Demo
+            <NavImg src='https://www.firstdecatur.org/wp-content/uploads/2019/07/Globe-Icon-3.png' title='' />
           </NavLink>
         </Link>
         <Link to='/space'>
           <NavLink>
-            Space
+            <NavImg src='https://upload.wikimedia.org/wikipedia/commons/d/d0/International_Space_Station.svg' />
           </NavLink>
         </Link>
       </Nav>
