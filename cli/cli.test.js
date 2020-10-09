f694161ea114718d4f6744d5e353e077bb6cd24d
Bilal Ather <ather.bilal@gmail.com>
2020-10-09 15:42:09 -0500

    fixed tests yet again

--- cli/cli.test.js
+++ cli/cli.test.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
+const path = require('path')
 
 describe('cli', () => {
     it('should verify app/demos/world exists', () => {
         try {
-            require('../app/demos/world/App.js')
-            require('../app/demos/world/index.js')
+            fs.accessSync('./app/demos/world/App.js', fs.F_OK);
+            fs.accessSync('./app/demos/world/index.js', fs.F_OK)
+            fs.accessSync('./app/demos/world/index.html', fs.F_OK)
             expect(true).toBe(true)
         }
         catch(e) {
