b236987a864c25dd166c527335fc190e3a2a7e20
Bilal Ather <ather.bilal@gmail.com>
2020-10-09 15:30:41 -0500

    fixed borked test

--- cli/cli.test.js
+++ cli/cli.test.js
@@ -4,12 +4,12 @@ describe('cli', () => {
     it('should verify app/demos/world exists', () => {
         try {
             require('../app/demos/world/App.js')
-            require('../app/demos/world/index.html')
             require('../app/demos/world/index.js')
             expect(true).toBe(true)
         }
         catch(e) {
             console.log('app/demos/world has been borked!!')
+            console.log(e)
             expect(false).toBe(true)
         }
     })
