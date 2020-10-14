445779d48ed7cf3d9a553036d1e06a2b5bae8a63
Bilal Ather <ather.bilal@gmail.com>
2020-10-14 13:36:45 -0500

    added using create-react-app

--- cli/generator/generate.js
+++ cli/generator/generate.js
@@ -5,7 +5,6 @@ const packageMerge = require('./packageMerge')
 const exec = require('child-process-promise').exec
 const depcheck = require('depcheck')
 const path = require("path")
-
 module.exports = {
   run: async () => {
     const projectDir = cliInput.get('projectDirectory')
@@ -13,35 +12,43 @@ module.exports = {
     let peerDepInstall = ''
     // Create directory for new implementing application
     if (!rootDir.has()) rootDir.make()
-
     return new Promise( async (resolve, reject) => {
       // Clone ol-kit directory into new implementing application
       console.log(chalk.cyan(`    Cloning ol-kit files into /${projectDir}`))
-      return exec(`git clone https://github.com/MonsantoCo/ol-kit.git ./${projectDir}`) 
+      return exec(`git clone --single-branch --branch cli https://github.com/MonsantoCo/ol-kit.git ./${projectDir}`)
       .then(() => {
         // If our peer dependencies ship a breaking change this will need to be addressed.
         const peerDependencies = require(`${rootDir.projectPath}/package.json`).peerDependencies
-
         // installing only major packages eg. if >=5.2.3 we will install package@5
         for (const [package, version] of Object.entries(peerDependencies)) {
           peerDepInstall += `${package}@${version.split('.')[0].replace(/\D/g,'')} `
         }
         return peerDepInstall
       })
-      .then((peerDepInstall) => {
-        console.log(chalk.cyan(`    Installing dependencies...(This may take a while)`))
+      .then(() => {
+        console.log(chalk.cyan(`    Configuring Map...(This may take a while)`))
         // remove everything except app/demos/world/*
         return exec(`cd ./${projectDir} && 
-        find . -depth -mindepth 1 \! -path "./app/demos/world/*" \! -path "./app/demos/world" -delete && 
-        mv app/demos/world/App.js . && 
-        rm -rf app && npm init -y &&
-        npm install ${peerDepInstall} --save`)
+        find . -depth -mindepth 1 \! -path "./app/demos/world/*" \! -path "./app/demos/world" \! -delete &&
+        mkdir temp &&
+        mv -v ./app/demos/world/* ./temp/ &&
+        rm -rf app &&
+        create-react-app ./react-temp &&
+        mv -v ./react-temp/* .
+        find . -depth -mindepth 1 \! -path "./node_modules/*" \! -path "./public/index.html" \! -path "./src" \! -path "./.gitignore" \! -path "./package*" \! -path "./temp/*" -delete &&
+        rm ./public/index.html
+        mv ./temp/index.html ./public/
+        mv ./temp/favicon.ico ./public/
+        mv -v ./temp/* ./src/ &&
+        rm -rf temp react-temp`)
       })
       .then(() => {
         //check dependencies to see if there are any missing then install them
         return depcheck(path.resolve(`./${projectDir}`), {}, (unused) => {
           const missingDependencies = Object.keys(unused.missing).join(' ')
-          return exec(`cd ${projectDir}; npm install ${missingDependencies} --save`, () => {
+          const usedDependencies = Object.keys(unused.using).join(' ')
+          console.log(chalk.cyan(`    Installing Dependencies...(This also may take a while)`))
+          return exec(`cd ${projectDir}; npm install ${missingDependencies} ${peerDepInstall} ${usedDependencies} --save`, () => {
             console.log(chalk.cyan('🌏  Success!'))
             console.log(chalk.cyan('🗺  Get started ->'))
             console.log(chalk.green(`    $ cd ${projectDir}`))
