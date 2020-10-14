bbab26708e3f9b5d89292c4a7f733d566e4c9641
Usman Ahmad Saeed <20966983+UsmanAhmadSaeed@users.noreply.github.com>
2020-10-15 01:14:30 +0500

    reverted styling change that broke app

--- src/Map/styled.js
+++ src/Map/styled.js
@@ -29,7 +29,7 @@ export const LogoText = styled.p`
 export const StyledMap = styled.div`
   height: 100%;
   width: 100%;
-  position: fixed;
+  position: ${p => p.fullScreen ? 'fixed' : 'relative'};
   top: 0;
   bottom: 0;
   left: 0;
