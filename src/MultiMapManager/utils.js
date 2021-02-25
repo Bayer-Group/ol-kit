7eb92d61f0e63143d39d0c43724ba061d4cae87a
PatchesMaps <patrick.moulden@gmail.com>
2021-02-25 16:48:44 -0500

    add some comments

--- src/MultiMapManager/utils.js
+++ src/MultiMapManager/utils.js
@@ -6,21 +6,14 @@ import Event from 'ol/events/Event'
  * @category MultiMap
  * @since 1.7.0
  * @param {Object} [views] - Array of openlayers views
- * @param {Object} [opts] - Object of optional params
- * @param {Object} [optSyncedViews] - Optional synced view override array
  */
-export function syncViewEvents (views, eventType, optSyncedViews) {
+export function syncViewEvents (views) {
   views.forEach((view, _, viewArray) => {
-    const syncedViews = optSyncedViews || viewArray
+    const syncedViews = viewArray
 
-    if (eventType) {
-      view.on(eventType, e => mapChangeHandler(e, syncedViews))
-    } else {
-      // default to all map view movement events
-      view.on('change:center', e => mapChangeHandler(e, syncedViews))
-      view.on('change:resolution', e => mapChangeHandler(e, syncedViews))
-      view.on('change:rotation', e => mapChangeHandler(e, syncedViews))
-    }
+    view.on('change:center', e => mapChangeHandler(e, syncedViews))
+    view.on('change:resolution', e => mapChangeHandler(e, syncedViews))
+    view.on('change:rotation', e => mapChangeHandler(e, syncedViews))
   })
 }
 
@@ -45,7 +38,7 @@ export function mapChangeHandler (event, syncedViews) {
           if (eventView.getInteracting()) {
             view.adjustCenter(eventView.getCenter().map((val, i) => val - event.oldValue[i]))
           } else if (event.key === 'resolution') {
-            view.setCenter(eventView.getCenter())
+            view.setCenter(eventView.getCenter()) // this could potentially sync the view centers rather than syncing the delta center
           }
           break
         case 'change:resolution':
