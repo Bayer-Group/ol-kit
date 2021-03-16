43c3280268c4bb1bad325f4aeb7846b513b42d4d
Patrick Moulden <patrick.moulden@gmail.com>
2021-03-16 11:05:53 -0400

    clean up some utils and commented code

--- src/GoogleDirections/GoogleDirections.js
+++ src/GoogleDirections/GoogleDirections.js
@@ -1,98 +1,13 @@
 import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
 import { connectToContext } from 'Provider'
-import { Draw, DrawPin, getVertices, pointsFromVertices } from '../Draw'
+import { Draw, DrawPin } from '../Draw'
 import { VectorLayer } from '../classes'
 import olSourceVector from 'ol/source/Vector'
-import olStyleFill from 'ol/style/Fill'
-import olStyleStroke from 'ol/style/Stroke'
-import olStyle from 'ol/style/Style'
-import olStyleCircle from 'ol/style/Circle'
 import olFormatPolyline from 'ol/format/Polyline'
 import { toLonLat } from 'ol/proj'
-import olGeomPoint from 'ol/geom/Point'
 import olGeomLineString from 'ol/geom/LineString'
-import olStyleIcon from 'ol/style/Icon'
-import tinycolor from 'tinycolor2'
-
-const mapPin = '<path d="M3 0c-1.66 0-3 1.34-3 3 0 2 3 5 3 5s3-3 3-5c0-1.66-1.34-3-3-3zm0 1c1.11 0 2 .9 2 2 0 1.11-.89 2-2 2-1.1 0-2-.89-2-2 0-1.1.9-2 2-2z" transform="translate(1)" />'
-
-function getSVGWrapper (svg, opts) {
-  const path = svg.path || svg
-  const viewBox = svg.viewBox || '0 0 8 8'
-  const {
-    stroke = {
-      color: '',
-      width: '',
-      opacity: 1
-    },
-    fill = '#e82f1c',
-    size = {
-      width: 8,
-      height: 8
-    }
-  } = opts
-
-  // Chrome 72+ doesn't allow hex fill colors for SVG -- we ensure fills & strokes are always RGB
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="${tinycolor(fill).toRgbString()}" stroke="${stroke.color ? tinycolor(stroke.color).toRgbString() : ''}" stroke-width="${stroke.width}" stroke-opacity="${stroke.opacity}" width="${size.width}" height="${size.height}" viewBox="${viewBox}" preserveAspectRatio="none">${path}</svg>`
-}
-
-function getSVGUri (path, opts = {}) {
-  return window.encodeURI(getSVGWrapper(path, opts))
-}
-
-const fill = new olStyleFill({
-  color: 'rgba(255,255,255,0.4)'
-})
-const stroke = new olStyleStroke({
-  color: '#3399CC',
-  width: 3.5
-})
-const routeStyle = new olStyle({
-  image: new olStyleCircle({
-    fill,
-    stroke,
-    radius: 5
-  }),
-  fill,
-  stroke
-})
-const waypointPin = new olStyle({
-  image: new olStyleCircle({
-    fill,
-    stroke: stroke,
-    radius: 5
-  }),
-  geometry: getVertices
-})
-
-const startPin = new olStyle({
-  image: new olStyleIcon({
-    anchor: [0.5, 0.5],
-    opacity: 1,
-    src: `data:image/svg+xml;utf8,${getSVGUri(mapPin, { fill: 'green', size: 8 })}`,
-    scale: 4
-  }),
-  geometry: feature => {
-    const geom = feature.getGeometry()
-    const points = pointsFromVertices(geom)
-
-    return new olGeomPoint(points[0])
-  }
-})
-const endPin = new olStyle({
-  image: new olStyleIcon({
-    anchor: [0.5, 0.5],
-    opacity: 1,
-    src: `data:image/svg+xml;utf8,${getSVGUri(mapPin, { fill: 'rgba(255,0,0,1)', size: 8 })}`,
-    scale: 4
-  }),
-  geometry: feature => {
-    const points = pointsFromVertices(feature.getGeometry())
-
-    return new olGeomPoint(points[points.length - 1])
-  }
-})
+import { startPin, endPin, waypointPin, routeStyle } from './utils'
 
 const getDirections = async (locations, apiKey) => {
   const waypoints = locations
---
+++ src/GoogleDirections/utils.js
@@ -0,0 +1,111 @@
+import olStyleFill from 'ol/style/Fill'
+import olStyleStroke from 'ol/style/Stroke'
+import olStyle from 'ol/style/Style'
+import olStyleCircle from 'ol/style/Circle'
+import { getVertices, pointsFromVertices } from '../Draw'
+import olGeomPoint from 'ol/geom/Point'
+import olStyleIcon from 'ol/style/Icon'
+import tinycolor from 'tinycolor2'
+
+const mapPin = '<path d="M3 0c-1.66 0-3 1.34-3 3 0 2 3 5 3 5s3-3 3-5c0-1.66-1.34-3-3-3zm0 1c1.11 0 2 .9 2 2 0 1.11-.89 2-2 2-1.1 0-2-.89-2-2 0-1.1.9-2 2-2z" transform="translate(1)" />'
+
+function getSVGWrapper (svg, opts) {
+  const path = svg.path || svg
+  const viewBox = svg.viewBox || '0 0 8 8'
+  const {
+    stroke = {
+      color: '',
+      width: '',
+      opacity: 1
+    },
+    fill = '#e82f1c',
+    size = {
+      width: 8,
+      height: 8
+    }
+  } = opts
+
+  // Chrome 72+ doesn't allow hex fill colors for SVG -- we ensure fills & strokes are always RGB
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="${tinycolor(fill).toRgbString()}" stroke="${stroke.color ? tinycolor(stroke.color).toRgbString() : ''}" stroke-width="${stroke.width}" stroke-opacity="${stroke.opacity}" width="${size.width}" height="${size.height}" viewBox="${viewBox}" preserveAspectRatio="none">${path}</svg>`
+}
+
+function getSVGUri (path, opts = {}) {
+  return window.encodeURI(getSVGWrapper(path, opts))
+}
+
+const fill = new olStyleFill({
+  color: 'rgba(255,255,255,0.4)'
+})
+
+const stroke = new olStyleStroke({
+  color: '#3399CC',
+  width: 3.5
+})
+
+export const routeStyle = new olStyle({
+  image: new olStyleCircle({
+    fill,
+    stroke,
+    radius: 5
+  }),
+  fill,
+  stroke
+})
+
+export const waypointPin = new olStyle({
+  image: new olStyleCircle({
+    fill,
+    stroke: stroke,
+    radius: 5
+  }),
+  geometry: getVertices
+})
+
+export const startPin = new olStyle({
+  // image: new olStyleCircle({
+  //   fill: new olStyleFill({
+  //     color: 'green'
+  //   }),
+  //   stroke: new olStyleStroke({
+  //     color: '#3399CC',
+  //     width: 0.5
+  //   }),
+  //   radius: 8
+  // }),
+  image: new olStyleIcon({
+    anchor: [0.5, 0.5],
+    opacity: 1,
+    src: `data:image/svg+xml;utf8,${getSVGUri(mapPin, { fill: 'green', size: 8 })}`,
+    scale: 4
+  }),
+  geometry: feature => {
+    const geom = feature.getGeometry()
+    const points = pointsFromVertices(geom)
+
+    return new olGeomPoint(points[0])
+  }
+})
+
+export const endPin = new olStyle({
+  // image: new olStyleCircle({
+  //   fill: new olStyleFill({
+  //     color: 'red'
+  //   }),
+  //   stroke: new olStyleStroke({
+  //     color: '#3399CC',
+  //     width: 0.5
+  //   }),
+  //   radius: 8
+  // }),
+  image: new olStyleIcon({
+    anchor: [0.5, 0.5],
+    opacity: 1,
+    src: `data:image/svg+xml;utf8,${getSVGUri(mapPin, { fill: 'rgba(255,0,0,1)', size: 8 })}`,
+    scale: 4
+  }),
+  geometry: feature => {
+    const points = pointsFromVertices(feature.getGeometry())
+
+    return points.length > 1 ? new olGeomPoint(points[points.length - 1]) : null
+  }
+})
