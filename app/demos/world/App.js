2bf9369162905f3f54202816cdfa1b6b48c5af30
Patrick Moulden <patrick.moulden@gmail.com>
2021-04-19 10:55:44 -0400

    revert app change

--- app/demos/world/App.js
+++ app/demos/world/App.js
@@ -23,25 +23,25 @@ import Welcome from '../../Welcome'
 class App extends React.Component {
   onMapInit = async (map) => {
     // create a vector layer and add to the map
-    // const layer = new VectorLayer({
-    //   title: '1904Labs HQ',
-    //   source: new olSourceVector({
-    //     features: [new olFeature({
-    //       feature_type: ['1904Labs HQ'],
-    //       title: '1904Labs HQ',
-    //       name: '1904Labs HQ',
-    //       geometry: new olGeomPoint(fromLonLat([-90.24618, 38.636069]))
-    //     })]
-    //   })
-    // })
+    const layer = new VectorLayer({
+      title: '1904Labs HQ',
+      source: new olSourceVector({
+        features: [new olFeature({
+          feature_type: ['1904Labs HQ'],
+          title: '1904Labs HQ',
+          name: '1904Labs HQ',
+          geometry: new olGeomPoint(fromLonLat([-90.24618, 38.636069]))
+        })]
+      })
+    })
 
-    // map.addLayer(layer)
+    map.addLayer(layer)
 
-    // const dataLayer = await loadDataLayer(map, 'https://data.nasa.gov/api/geospatial/7zbq-j77a?method=export&format=KML')
+    const dataLayer = await loadDataLayer(map, 'https://data.nasa.gov/api/geospatial/7zbq-j77a?method=export&format=KML')
 
-    // dataLayer.getSource().getFeatures().forEach(f => f.set('title', f.get('name')))
+    dataLayer.getSource().getFeatures().forEach(f => f.set('title', f.get('name')))
 
-    window.olMap = map
+    window.map = map
   }
 
   render () {
