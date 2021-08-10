9a4fe90352f226e4b3b99755306ea78be715f617
Kevin Gunn <kgunn@WUST035647.bayer.cnb>
2021-08-10 15:59:55 -0500

    component for zoom

--- src/Map/utils.js
+++ src/Map/utils.js
@@ -10,6 +10,7 @@ import olStyle from 'ol/style/Style'
 import olStroke from 'ol/style/Stroke'
 import VectorLayer from 'ol/layer/Vector'
 import VectorSource from 'ol/source/Vector'
+import Extent from 'ol/extent'
 import qs from 'qs'
 
 import ugh from 'ugh'
@@ -239,4 +240,26 @@ export function getSelectInteraction (map, name = DEFAULT_SELECT_NAME) {
   if (!select) return ugh.throw(`Select interaction with name '${name}' could not be found on the map`)
 
   return select
+}
+
+/**
+ * Sets the map extent to the given values.
+ *
+ * @param {ol.Map} map Open Layers map
+ * @param {ol.Extent} extent New extent definition
+ */
+export function setMapExtent (map, extent) {
+  map.getView().fit(extent, map.getSize())
+}
+
+/**
+ * Calculates a map extent that would make all features in the list visible.
+ *
+ * @param {ol.Feature[]} featureList List of Open Layers features
+ * @returns {ol.Extent} Map extent
+ */
+export function getExtentForFeatures (featureList) {
+  const extent = featureList.reduce((acc, f) => Extent.extend(acc, f.getGeometry().getExtent()), Extent.createEmpty())
+
+  return extent
 }
---
+++ src/Popup/PopupActions/PopupActionZoomToExtent/PopupActionZoomToExtent.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { PopupActionItem } from 'Popup'
+import { connectToContext } from 'Provider'
+import { setMapExtent, getExtentForFeatures } from 'Map'
+
+class ActionZoomToExtent extends Component {
+  zoom = () => {
+    const { feature, map } = this.props
+
+    setMapExtent(map, getExtentForFeatures([feature]))
+  }
+
+  render () {
+    const { translations } = this.props
+
+    return (
+      <PopupActionItem
+        title={translations['_ol_kit.PopupActionZoomToExtent.title']}
+        onClick={() => this.zoom()}
+        style={{ color: '#c0392b' }} />
+    )
+  }
+}
+
+ActionZoomToExtent.propTypes = {
+  feature: PropTypes.object,
+  /** olMap */
+  map: PropTypes.object,
+  translations: PropTypes.shape({
+    '_ol_kit.PopupActionZoomToExtent.title': PropTypes.string
+  }).isRequired
+}
+
+ActionZoomToExtent.defaultProps = {
+  translations: {
+    '_ol_kit.PopupActionZoomToExtent.title': 'Zoom to Extent'
+  }
+}
+
+export default connectToContext(ActionZoomToExtent)
