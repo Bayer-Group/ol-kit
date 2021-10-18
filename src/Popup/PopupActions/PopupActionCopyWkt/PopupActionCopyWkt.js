b0af040e73a8cdc2373c589c9cd3b226b3dab39f
Daniel Rodenberg <drodenberg92@gmail.com>
2021-10-18 10:28:05 -0500

    added a snackbar message for wkt copy

--- src/Popup/PopupActions/PopupActionCopyWkt/PopupActionCopyWkt.js
+++ src/Popup/PopupActions/PopupActionCopyWkt/PopupActionCopyWkt.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { connectToContext } from 'Provider'
 import { PopupActionItem } from 'Popup'
 import en from 'locales/en'
 import { copyWktToClipbard } from './utils'
+import { Snackbar } from 'Snackbar'
 
 /**
  * Popup action item to copy geometry as WKT to clipboard
  * @component
  * @category PopupActionCopyWkt
  */
-const PopupActionCopyWkt = ({ feature, translations, decimalPlaces = 5 }) => (
-  <PopupActionItem
-    title={translations['_ol_kit.PopupActionWkt.copyToClipboard']}
-    onClick={() => copyWktToClipbard(feature, decimalPlaces)} />
-)
+const PopupActionCopyWkt = ({ feature, translations, decimalPlaces = 5 }) => {
+  const [showSnackbar, setShowSnackbar] = useState(false)
+
+  const onCopyWktToClipboard = () => {
+    setShowSnackbar(true)
+
+    copyWktToClipbard(feature, decimalPlaces)
+  }
+
+  return <>
+    <PopupActionItem
+      title={translations['_ol_kit.PopupActionWkt.copyToClipboard']}
+      onClick={() => onCopyWktToClipboard()} />
+    <Snackbar
+      open={showSnackbar}
+      closeSnackbar={() => setShowSnackbar(false)}
+      duration={5000}
+      message={'Wkt copied to clipboard!'}
+      variant='info'/>
+  </>
+}
 
 PopupActionCopyWkt.propTypes = {
   /** The number of decimal places in the output WKT coords */
